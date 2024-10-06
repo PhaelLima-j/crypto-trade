a4158da3ed2c6c5c28be6c61e245994e2ae9598f
Raphael Lima <rapha.lima.rl16@gmail.com>
2024-10-06 17:54:03 -0300

    Criando rota para cadastro de usuário

---
+++ models/usuario.js
@@ -0,0 +1,40 @@
+const { Schema } = require('mongoose');
+const { cpf } = require('cpf-cnpj-validator');
+
+const Usuario = new Schema({
+    nome: {
+        type: String,
+        required: true,
+        min: 4,
+    },
+    cpf: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function (v) {
+                return cpf.isValid(v);
+            },
+            message: props => `${props.value} não é um cpf válido`
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 4,
+        unique: true,
+        validate: {
+            validator: function (v) {
+                return v.match('@');
+            },
+            message: props => `${props.value} não é um email válido`
+        },
+    },
+    senha: {
+        type: String,
+        required: true,
+        select: false,
+    },
+})
+
+module.exports = Usuario;
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const statusRouter = require('./status');
+const usuariosRouter = require('./usuarios')
 
 const router = express.Router();
 
+router.use('/usuarios', usuariosRouter);
 router.use('/status', statusRouter);
 
 module.exports = router;
